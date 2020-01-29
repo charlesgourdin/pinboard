76bd17403ee6d401cd7cc28956ef49ef768f7851
Charles Gourdin <gourdin.charles@gmail.com>
2020-01-29 13:44:29 +0100

    Mise en place des composants react et initialisation d'express

---
+++ back/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+const port = 4000
+
+//Configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    console.log(req.method, req.originalUrl)
+    next()
+})
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Express fonctionne')
+})
+
+//Server
+app.listen(port, (err) => {
+    if (err) {
+        throw new Error("serveur don't listen")
+    }
+    console.log(`Server is listening on port ${port}`)
+})
